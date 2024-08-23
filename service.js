807339552d0232782fb394caddcc75885b468076
dev <dev@broadwayinc.com>
2024-08-23 12:53:22 +0000

    update

--- service.js
+++ service.js
@@ -8,11 +8,8 @@
     Make sure you replace the first, and the second argument: "ap220wfRHl9Cw2QqeFEc", "f8e16604-69e4-451c-9d90-4410f801c006" to your own service ID and owner ID.
     You can retrieve the service ID and the owner ID from www.skapi.com.
     For more information, checkout the Getting Started: https://docs.skapi.com/introduction/getting-started.html
-
-    The third argument we are passing is an object with a property called autoLogin.
-    If autoLogin is set to true, then the user will always be automatically logged in when the page loads.
 */
-const skapi = new Skapi("ap220wfRHl9Cw2QqeFEc", "f8e16604-69e4-451c-9d90-4410f801c006", { autoLogin: true });
+const skapi = new Skapi("ap220wfRHl9Cw2QqeFEc", "f8e16604-69e4-451c-9d90-4410f801c006");
 
 /*
     The following function disableForm() is for disabling the form while the user is submitting.
@@ -21,8 +18,7 @@ const skapi = new Skapi("ap220wfRHl9Cw2QqeFEc", "f8e16604-69e4-451c-9d90-4410f80
 */
 function disableForm(form, disabled) {
     form.querySelectorAll('input').forEach(input => {
-        if (input.type !== 'radio' && input.type !== 'checkbox')
-            input.disabled = disabled
+        input.disabled = disabled;
     });
 
     form.querySelectorAll('a').forEach(a => {
